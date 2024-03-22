02bf6497c8b06c32b1d6f5a3fdba9bfb94bcabf8
Bright-Anyawe <157108952+Bright-Anyawe@users.noreply.github.com>
2024-03-22 05:50:42 +0000

    Add variable to store input
    This been done the operate() is called on the
     click the eventlistener of the assign key to perform the operation.

--- README.md
+++ README.md
@@ -17,3 +17,7 @@ divide
  
  - Create the functions that populate the display when you click the number buttons.
   You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+
+- Make the calculator work! You’ll need to store the first number and second number
+ that are input into the calculator, utilize the operator that the user selects,
+  and then operate() on the two numbers when the user presses the “=” key.
--- calculator.js
+++ calculator.js
@@ -1,20 +1,23 @@
+let a = '';
+let b = '';
+let operator;
 
 const add = (a, b) => {
-    return a + b;
+    return parseInt(a) + parseInt(b);
 };
 
 const subtract = (a, b) => {
-    return a - b;
+    return parseInt(a) - parseInt(b);
 };
 
 const multiply = (a, b) => {
-    return a * b;
+    return parseInt(a) * parseInt(b);
 };
 
 const divide = (a, b) => {
-    return a / b;
+    return parseInt(a) / parseInt(b);
 };
-let a, b, operator;
+
 
 
 const operate = (a, b, operator) => {
@@ -33,18 +36,58 @@ console.log(operate(3, 4, "*"));
 
 
 const container = document.querySelector('#container');
-const result = document.querySelector('#result');
-const buttonsDigit = document.querySelectorAll('#digits button');
+const displayResult = document.querySelector('#result');
+const buttonsOperator = document.querySelectorAll('#operators button');
+const buttonsNumber = document.querySelectorAll('#digits button');
+const equalAssign = document.querySelector('#assign');
+const clear = document.querySelector('#Clear');
 
-    
-    buttonsDigit.forEach((button) => {
-        button.addEventListener('click', (e) => {
-        const firstNumber = result.textContent += e.target.textContent;
+
+buttonsNumber.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        displayResult.textContent += e.target.textContent;
+
+        if (!operator) {
+            a += e.target.textContent;
+            console.log(a);
+        }   
+        else {
+            b += e.target.textContent;
+            console.log(b);
+        }
     });
+});
+
+
+buttonsOperator.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        operator = displayResult.textContent = e.target.textContent;
+        console.log(operator)
     });
+});
+
+equalAssign.addEventListener('click', () => {
+    displayResult.textContent = operate(a,b,operator);
+});
 
 
 
 
 
+
+
+
+
+// buttonsNumber.forEach((button) => {
+//     button.addEventListener('click', (e) => {
+//         if (!operator) {
+//             a += e.target.textContent;
+//             console.log(a);
+//         }   
+//         else {
+//             b += e.target.textContent;
+//             console.log(b);
+//         }
+//     })
+// })
 
